fac4cbcdb14f4a21019a75402038659d56ac3ef2
Ravi Shankar Saini <sainiravi015@gmail.com>
2025-09-01 19:21:58 +0530

    fix type error when mount plugin

--- src/mountPlugin.ts
+++ src/mountPlugin.ts
@@ -1,12 +1,15 @@
 import type { SvelteComponent } from 'svelte';
-import { ShapeType, type ImageAnnotator } from '@annotorious/annotorious';
+import { ShapeType, type ImageAnnotator, type ImageAnnotation, type Annotation } from '@annotorious/annotorious';
 import type { OpenSeadragonAnnotator } from '@annotorious/openseadragon';
 import { EllipseEditor, RubberbandEllipse } from './ellipse';
 import { LineEditor, RubberbandLine } from './line';
 import { RubberbandPath, PathEditor } from './path';
 
-export const mountPlugin = (
-  anno: ImageAnnotator | OpenSeadragonAnnotator
+export const mountPlugin = <
+  I extends Annotation = ImageAnnotation,
+  E extends unknown = ImageAnnotation,
+>(
+  anno: ImageAnnotator<I, E> | OpenSeadragonAnnotator<I, E>
 ) => {
   anno.registerDrawingTool('ellipse', RubberbandEllipse as typeof SvelteComponent);
   anno.registerShapeEditor(ShapeType.ELLIPSE, EllipseEditor as typeof SvelteComponent);
